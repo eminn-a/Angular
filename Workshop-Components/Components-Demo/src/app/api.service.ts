c6d96bcebb1fa425da5a1997eb1eeaa80a8e8d7f
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-03-09 11:31:38 +0200

    get limited posts-working!

--- Workshop-Components/Components-Demo/src/app/api.service.ts
+++ Workshop-Components/Components-Demo/src/app/api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { Theme } from './types/theme';
+import { Post } from './types/post';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +12,16 @@ export class ApiService {
 
   getThemes() {
     const { apiUrl } = environment;
-    return this.http.get<Theme>(`${apiUrl}/themes`);
+    return this.http.get<Theme[]>(`${apiUrl}/themes`);
   }
 
-  getPosts(limit?: number) {}
+  getPosts(limit?: number) {
+    const { apiUrl } = environment;
+    let url = `${apiUrl}/posts`;
+
+    if (limit) {
+      url += `?limit=${limit}`;
+    }
+    return this.http.get<Post[]>(url);
+  }
 }
