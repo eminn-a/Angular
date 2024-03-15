ee01e857293b0efb22a73c4d502f0333fc5849c8
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-03-15 11:08:55 -0700

    added auth gard for add theme

--- Workshop-Components/Components-Demo/src/app/theme/theme-routing.module.ts
+++ Workshop-Components/Components-Demo/src/app/theme/theme-routing.module.ts
@@ -4,10 +4,15 @@ import { MainComponent } from '../main/main.component';
 import { AddThemeComponent } from './add-theme/add-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
 import { HomeComponent } from '../home/home.component';
+import { AuthActivate } from '../guards/auth.activate';
 
 const routes: Routes = [
   { path: 'themes', component: MainComponent },
-  { path: 'add-theme', component: AddThemeComponent },
+  {
+    path: 'add-theme',
+    component: AddThemeComponent,
+    canActivate: [AuthActivate],
+  },
   { path: 'home', component: HomeComponent },
 ];
 
